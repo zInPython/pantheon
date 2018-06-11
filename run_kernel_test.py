cf3653d3beb56b270c12b29cdeb3262c81c26a71
Nathan Jay <njay2@illinois.edu>
2018-06-11 16:29:35 -0500

    Fixed string order.

--- run_kernel_test.py
+++ run_kernel_test.py
@@ -20,7 +20,7 @@ mm_opts = "--uplink-queue=droptail --uplink-queue-args=bytes=" + str(1000 * buf)
 cmd = "python gen_trace.py --bw=%d" % bw
 os.system(cmd)
 
-cmd = "./test/test.py local --schemes \"%s\" -t 30 --interval 15 --run-times 1 --flows %d --uplink-trace %s --downlink-trace %s --prepend-mm-cmds \"%s\" --extra-mm-link-args \"%s\"" % (n_flows, schemes, "link.trace", "link.trace", mm_cmds, mm_opts)
+cmd = "./test/test.py local --schemes \"%s\" -t 30 --interval 15 --run-times 1 --flows %d --uplink-trace %s --downlink-trace %s --prepend-mm-cmds \"%s\" --extra-mm-link-args \"%s\"" % (schemes, n_flows, "link.trace", "link.trace", mm_cmds, mm_opts)
 os.system(cmd)
 
 cmd = "./analysis/analyze.py --data-dir test/data/"
